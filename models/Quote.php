a04e71b850bff67955ce19323790c2d0ceb73bca
Nicholas La Claire <nlaclaire@yahoo.com>
2025-03-18 15:50:31 -0500

    Revert

--- models/Quote.php
+++ models/Quote.php
@@ -18,20 +18,18 @@
 
         public function create() 
         {
-            $query = 'INSERT INTO ' . $this->table . ' (id, quote, author_id, category_id) 
-            VALUES (:id, :quote, :author_id, :category_id)';
+            $query = 'INSERT INTO ' . $this->table . ' (quote, author_id, category_id) 
+            VALUES (:quote, :author_id, :category_id)';
   
             // Prepare Statement
             $stmt = $this->conn->prepare($query);
             
             // Clean data
-            $this->id = htmlspecialchars(strip_tags($this->id));
             $this->quote = htmlspecialchars(strip_tags($this->quote));
             $this->author_id = htmlspecialchars(strip_tags($this->author_id));
             $this->category_id = htmlspecialchars(strip_tags($this->category_id));
             
             // Bind data
-            $stmt->bindParam(':id', $this->id);
             $stmt->bindParam(':quote', $this->quote);
             $stmt->bindParam(':author_id', $this->author_id);
             $stmt->bindParam(':category_id', $this->category_id);
