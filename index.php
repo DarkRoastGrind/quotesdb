e5c3cc68670ee7546ff0471efa3dd26749f909b3
Nicholas La Claire <nlaclaire@yahoo.com>
2025-03-26 20:13:27 -0500

    Trying another way.

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 declare(strict_types = 1);
-// Enable CORS and set Content-Type
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
 // Get the HTTP request method and request URI
 $method = $_SERVER['REQUEST_METHOD'];
 $request_uri = $_SERVER['REQUEST_URI'];
@@ -30,6 +26,9 @@ if (!isset($segments[1]) || $segments[1] === '') {
 }
 
 $resource = $segments[1];
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
 // Define base paths for resources
 $base_path = __DIR__ . '/api';
