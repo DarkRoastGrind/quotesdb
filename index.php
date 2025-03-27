a9038d830bc098a711d3251a117b8ef6d824bb93
Nicholas La Claire <nlaclaire@yahoo.com>
2025-03-26 19:53:03 -0500

    Fixed strict type error

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 // Enable CORS and set Content-Type
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
@@ -13,8 +14,6 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     exit();
 }
-
-declare(strict_types = 1);
 
 echo '<pre>';
 print_r(getenv('SITE_URL'));
